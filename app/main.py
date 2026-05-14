95d33ed3612ce7218e867ab4bfd92b5023ceb57b
Fedora-Notebook-Michael <michael.moura72@hotmail.com>
2026-05-14 00:31:29 -0300

    :tada: create infra

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Docflow engine",
+    version="1.0.0"
+)
+
+@app.get("/")
+async def root():
+
+    return ("message": "Docflow Engine runnig")
